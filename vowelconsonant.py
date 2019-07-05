4f7ddfc0d09d7d9d3ef1476377caa335a9b044bb
Bramhendra Kumar <37215456+bramhendra123@users.noreply.github.com>
2019-07-05 11:29:14 +0530

    Update vowelconsonant.py

--- vowelconsonant.py
+++ vowelconsonant.py
@@ -1,6 +1,8 @@
 ch=input("Enter your own Character:)
 if(ch == 'a' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u' or ch == 'A'
        or ch == 'E' or ch == 'I' or ch == 'O' or ch == 'U'):
-       print("valid")
-else:
+       print("vowel")
+elif ((ch>='a') and (ch<='z')):
+      print("consonant")
+else :
       print("invalid")
