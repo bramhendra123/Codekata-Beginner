62a7dc14401c3e7567398a5bb041dfaa9ef44e05
Bramhendra Kumar <37215456+bramhendra123@users.noreply.github.com>
2019-07-08 16:28:43 +0530

    Update numbertest.py

--- numbertest.py
+++ numbertest.py
@@ -1,8 +1,5 @@
-n = float(input())
-if n<0:
-    print("negative")
-elif n>0:
-        print("positive")
-else:
-        print("Zero")
-            
+san=int(input())
+sum=0
+for i in range(1,san+1):
+  sum=sum+i
+print(sum)
